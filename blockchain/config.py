e14ab71bba68b7b68e6a4045cfa53e6a434b1dcd
Abdoulaye MAIGA <maigaabdoulaye420@gmail.com>
2024-11-17 13:20:24 +0000

    Correction de is_connect

--- blockchain/config.py
+++ blockchain/config.py
@@ -18,9 +18,9 @@ if not SEPOLIA_URL:
 
 w3 = Web3(Web3.HTTPProvider(SEPOLIA_URL))
 # Vérification de la connexion à la blockchain
-if not w3.isConnected():
+if not w3.is_connected():
     raise Exception("Connexion au réseau Sepolia échouée.")
-print("Connecté à Sepolia :", w3.isConnected())
+print("Connecté à Sepolia :", w3.is_connected())
 
 
 
